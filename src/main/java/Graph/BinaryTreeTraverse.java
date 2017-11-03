d28afe4003953f06e2cc126c4cd7a0f4af888a01
ardal <ardal@DESKTOP-ALKQKIO>
2017-11-02 22:19:22 -0700

    added 2 algorithms for finding LCA: 1) path from root. 2) root subtrees

--- src/main/java/Graph/BinaryTreeTraverse.java
+++ src/main/java/Graph/BinaryTreeTraverse.java
@@ -8,6 +8,7 @@ package Graph;
 import LinkedList.LinkedList;
 import Queue.Queue;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  *
@@ -81,5 +82,126 @@ public class BinaryTreeTraverse<T> {
         return _nodeList;
     }
     
+    /**
+     * Returns the next node in the in-order traversal, or null if it is the last node
+     * of the traversal.
+     * @param node
+     * @return 
+     */
+    public BinaryTreeNode<T> getNextInOrderNode(BinaryTreeNode<T> node) {
+        if(node != null) {
+            BinaryTreeNode<T> rightChild = node.getRight();
+
+            if(rightChild != null) {
+                BinaryTreeNode<T> leftMostChild = rightChild;
+
+                while(leftMostChild.getLeft() != null) {
+                    leftMostChild = leftMostChild.getLeft();
+                }
+
+                return leftMostChild;
+            } else {
+                BinaryTreeNode<T> currentNode = node.parent;
+
+                while(currentNode!= null) {
+                    if (currentNode.parent != null && currentNode.parent.getLeft() == currentNode) {
+                        return currentNode.parent;
+                    }
+
+                    currentNode = currentNode.parent;
+                }
+
+                return currentNode;
+            }
+        }
+        
+        return null;
+    }
+    
+    /**
+     * 
+     * @param node1
+     * @param node2
+     * @return null if no common ancestor is found
+     */
+    public BinaryTreeNode<T> findLCA(BinaryTreeNode<T> node1, BinaryTreeNode<T> node2){
+        // if a node is root, it will be the ancestor
+        if(node1.parent == null) {
+            return node1;
+        } 
+        
+        if(node2.parent == null) {
+            return node2;
+        }
+        
+        HashSet<BinaryTreeNode<T>> ancestors = new HashSet<>();
+        
+        // Get all the ancestors of node1 all the way to the root
+        BinaryTreeNode<T> currentNode = node1;
+        while(currentNode != null) {
+            ancestors.add(currentNode);
+            
+            currentNode = currentNode.parent;
+        }
+        
+        // Go through the ancestors of node2 from the node to the root.
+        // The first node that is found in node1's ancestors is their lowest common ancestor
+        currentNode = node2;
+        while(currentNode != null){
+            if(ancestors.contains(currentNode)) {
+                return currentNode;
+            }
+            
+            currentNode = currentNode.parent;
+        }
+        
+        return null;
+    }
+    
+    /**
+     * This method checks if node1 and node2 are in the same subtree of root.
+     * This algorithm is for when we cannot access parents from a given node.
+     * @param node1
+     * @param node2
+     * @param root
+     * @return 
+     */
+    public BinaryTreeNode<T> findLCAByRoot(BinaryTreeNode<T> node1, BinaryTreeNode<T> node2, BinaryTreeNode<T> root) {
+        if(node1 == root) {
+            return node1;
+        }
+        
+        if(node2 == root) {
+            return node2;
+        }
+        
+        boolean isNode1ALeftDescendant = isDescendant(root.getLeft(), node1);
+        boolean isNode2ALeftDescendant = isDescendant(root.getLeft(), node2);
+        
+        // The root is the LCA when one node is in the left sub tree and the other in the right sub tree
+        if(isNode1ALeftDescendant != isNode2ALeftDescendant) {
+            return root;
+        }
+        
+        if(isNode1ALeftDescendant) {
+            return findLCAByRoot(node1, node2, root.getLeft());
+        }
+        else{
+            return findLCAByRoot(node1,node2,root.getRight());
+        }
+    }
+    
+    public boolean isDescendant(BinaryTreeNode<T> root, BinaryTreeNode<T> node) {
+        // we've reached a leaf - the node is not a descendant.
+        if(root == null) {
+            return false;
+        }
+        
+        if(root == node) {
+            return true;
+        }
+        
+        return isDescendant(root.getLeft(), node) || isDescendant(root.getRight(), node);
+    }
 }
 
--- src/test/java/Graph/BinaryTreeTraverseTest.java
+++ src/test/java/Graph/BinaryTreeTraverseTest.java
@@ -5,7 +5,6 @@
  */
 package Graph;
 
-import LinkedList.LinkedList;
 import java.util.ArrayList;
 import org.junit.*;
 import static org.junit.Assert.*;
@@ -68,4 +67,169 @@ public class BinaryTreeTraverseTest {
         assertEquals("[5, 3, 7, 2, 5, 8]", results.toString());
     }
     
+    @Test
+    public void getNextInOrderNode_returns_leftmost_node_in_right_subtree_when_node_has_a_right_child() {
+        // Set up
+        BinaryTree<Integer> tree = TestDataCreator.createTestBST();
+        BinaryTreeTraverse<Integer> sut = new BinaryTreeTraverse<>();
+        
+        // Execute
+        BinaryTreeNode<Integer> result = sut.getNextInOrderNode(tree.root.getLeft());
+        
+        // Assert
+        assertEquals(7, (int)result.data);
+    }
+    
+    @Test
+    public void getNextInOrderNode_with_right_child_node_returns_parents_next_node() {
+        // Set up
+        BinaryTree<Integer> tree = new BinaryTree<Integer>();
+        tree.root.data = 10;
+        
+        BinaryTreeNode<Integer> node15 = new BinaryTreeNode<>(15);
+        tree.root.setRight(node15);
+        
+        BinaryTreeNode<Integer> node20 = new BinaryTreeNode<>(20);
+        node15.setLeft(node20);
+        
+        BinaryTreeNode<Integer> node21 = new BinaryTreeNode<>(21);
+        node20.setLeft(node21);
+        
+        BinaryTreeNode<Integer> node22 = new BinaryTreeNode<>(22);
+        node20.setRight(node22);
+        
+        BinaryTreeTraverse<Integer> sut = new BinaryTreeTraverse<>();
+        
+        // Execute
+        BinaryTreeNode<Integer> result = sut.getNextInOrderNode(node22);
+        
+        // Assert
+        assertEquals(15, (int)result.data);
+    }
+    
+    @Test
+    public void getNextInOrderNode_with_rightmost_node_returns_null() {
+        // Set up
+        BinaryTree<Integer> tree = TestDataCreator.createTestBST();
+        BinaryTreeTraverse<Integer> sut = new BinaryTreeTraverse<>();
+        BinaryTreeNode<Integer> node = tree.root.getRight().getRight();
+        
+        // Execute
+        BinaryTreeNode<Integer> result = sut.getNextInOrderNode(node);
+        
+        // Assert
+        assertNull(result);
+    }
+    
+    @Test
+    public void findLCA_finds_lowest_common_ancestor_of_two_nodes(){
+        // Set up
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>();
+        
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
+        root.setRight(node2);
+        
+        BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(3);
+        node2.setLeft(node3);
+        
+        BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(4);
+        BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(5);
+        node3.setLeft(node4);
+        node3.setRight(node5);
+        
+        BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
+        node4.setLeft(node6);
+        
+        BinaryTreeTraverse<Integer> sut = new BinaryTreeTraverse<>();
+        
+        // Execute
+        BinaryTreeNode<Integer> ancestor = sut.findLCA(node6, node5);
+        
+        // Assert
+        assertEquals(node3, ancestor);
+    }
+    
+    @Test
+    public void findLCA_finds_lowest_common_ancestor_of_root_and_child(){
+        // Set up
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>();
+        
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
+        root.setRight(node2);
+        
+        BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(3);
+        node2.setLeft(node3);
+        
+        BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(4);
+        BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(5);
+        node3.setLeft(node4);
+        node3.setRight(node5);
+        
+        BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
+        node4.setLeft(node6);
+        
+        BinaryTreeTraverse<Integer> sut = new BinaryTreeTraverse<>();
+        
+        // Execute
+        BinaryTreeNode<Integer> ancestor = sut.findLCA(node4, root);
+        
+        // Assert
+        assertEquals(root, ancestor);
+    }
+    
+    @Test
+    public void findLCAByRoot_finds_lowest_common_ancestor_of_two_nodes(){
+        // Set up
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>();
+        
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
+        root.setRight(node2);
+        
+        BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(3);
+        node2.setLeft(node3);
+        
+        BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(4);
+        BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(5);
+        node3.setLeft(node4);
+        node3.setRight(node5);
+        
+        BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
+        node4.setLeft(node6);
+        
+        BinaryTreeTraverse<Integer> sut = new BinaryTreeTraverse<>();
+        
+        // Execute
+        BinaryTreeNode<Integer> ancestor = sut.findLCAByRoot(node6, node5, root);
+        
+        // Assert
+        assertEquals(node3, ancestor);
+    }
+    
+    @Test
+    public void findLCAByRoot_finds_lowest_common_ancestor_of_root_and_child(){
+        // Set up
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>();
+        
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(2);
+        root.setRight(node2);
+        
+        BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(3);
+        node2.setLeft(node3);
+        
+        BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(4);
+        BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(5);
+        node3.setLeft(node4);
+        node3.setRight(node5);
+        
+        BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(6);
+        node4.setLeft(node6);
+        
+        BinaryTreeTraverse<Integer> sut = new BinaryTreeTraverse<>();
+        
+        // Execute
+        BinaryTreeNode<Integer> ancestor = sut.findLCAByRoot(node4, root, root);
+        
+        // Assert
+        assertEquals(root, ancestor);
+    }
 }
